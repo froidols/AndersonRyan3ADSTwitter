648800487b3d05b1cf4c86f5066e6e7b5d0fc591
Aluno <Aluno@INFO12>
2019-08-22 08:50:09 -0300

    primeirocommit

--- index.php
+++ index.php
@@ -1,17 +1,89 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <title></title>
-    </head>
+        
+        <title>Twitter clone</title>
+        <!-- link do Jquery -->
+        <script src="https//code.jquery.com/jquery-2.2.4.min.js"></script>
+        <!-- link do Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap.min.css">
+        
+        <script>
+            // codigo javascript
+            </script>
+            </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+       <!-- Static navbar -->
+       <nav class="navbar navbar-default navbar-static-top">
+           <div class="container">
+               <div class="navbar-header">
+                   <!-- ERRO -->
+                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar" aria-expander="false" aria-controls="navbar">
+                       <span class="sr-only">Toggle navigation</span>
+                       <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                   </button>
+    <img src="imagens/icone_twitter.png" />
+               </div>
+               <div id="navbar" class="navbar-collapse collapse">
+               <ul class="nav navbar-nav navbar-right">   
+                   <li><a href="inscrevase.php">Inscrever-se</a></li>
+                   <li class="">
+                       <a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
+                       <ul class="dropdown-menu" aria-labelledby="entrar">
+               <div class="col-md-12">
+                   <h3>Você possui uma conta?</h3>
+                       <br/>
+                       <form method="post" action="" id="formLogin">
+                           <div class="form-group">
+                               <input type="text" class="form-control" id="campo_usuario" name="usuario"
+                                      placeholder="Usuário"/>
+                           </div>
+                           
+                           <div class="form-group">
+                               <input type="password" class="form-control red" id="campo_senha" name="senha"
+                                      placeholder="Senha"/>
+                           </div>
+                           
+                           <button type="button" class="btn btn-primary" id="btn_login">Entrar</button>
+                           
+                           <br/> <br/>
+                           
+                       </form>
+                       </ul>
+                   </li>
+               </ul>
+</div><!--/.nav-collapse -->
+</div>
+       </nav>
+       
+       <div class="container">
+           
+           <div class="jumbotron">
+               <h1>Bem vindo ao twitter clone</h1>
+               <p>Veja o que está acontencendo agora...</p>
+           </div>
+           
+           <div class="clearfix"></div>
+       </div>
+       
+       
+       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </body>
+</html>
+           
+                           
+                       
+   
+                   
+                   
+                   
+                   
+                   
+                   
+               
+               </body>
+
 </html>
---
+++ registra_usuaruio.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'db.class.php';
+$usuario = $_POST['usuario'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$conec = new db ();
+
+$sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+
+//executar a query
+if(mysqli_query($link, $sql)){
+    echo "Usuário registrado com sucesso!";
+} else {
+    echo "Erro ao registar o usuário";
+}
+
+?>
